259bb42452b24e9b2d3bbdfad2a9309e35d860c6
EunHee Jang <ookk0215@naver.com>
2023-01-04 19:06:01 +0900

    ui 변경, ui변경에 따른 파이썬 코드 수정. jejuForm 파이썬코드와 ui 올립니다.

--- jeju.py
+++ jeju.py
@@ -1,8 +1,8 @@
 import pymysql
 import sys
-import csv
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+from jejuForm import Add
 
 form_class = uic.loadUiType("jeju.ui")[0]  # ui연결
 
@@ -11,31 +11,50 @@ class MainWindow(QMainWindow, form_class): #화면을 띄우는데 사용되는 
         super().__init__()
         self.setupUi(self)
         self.table_widget_create()
+        self.btn_search.clicked.connect(self.search)
+
 
 
     def table_widget_create(self):
-        conn = pymysql.connect(host='localhost', user='root', password='160072', db='jejudo', charset='utf8')
-
-        # Connection 으로부터 Cursor 생성
+        ## sql파일 커넥트
+        conn = pymysql.connect(host='localhost', user='root', password='0000', db='jejudo', charset='utf8')   # password 변경 해주세요
+        ## conn로부터  결과를 얻어올 때 사용할 Cursor 생성
         cur = conn.cursor()
-
-        # SQL문 실행
+        ## SQL문 실행
         sql = "select * from jejudo.jeju_table"
         cur.execute(sql)
-
-        # 데이타 Fetch
-        row = cur.fetchone()
+        print(cur.execute(sql))   # 실행(excute) 했더니 10884줄이 나온다.
+        ## 데이타 Fetch
+        # row = cur.fetchone()
+        # print(row) # ('카페송키','일반음식점..) 한 행이 튜플형태로 나온다.
         rows = cur.fetchall()
+        # print(rows) # 튜플안에 튜플로 전체 데이터를 불러온다.
+        self.table.setRowCount(len(rows)) # 테이블의 행 갯수를 rows의 길이로 정함
+        self.table.setColumnCount(len(rows[0]))  # 테이블의 열 갯수를 row의 길이로 정함
+
         for i in range(len(rows)):
-            for j in range(len(row)):
-                self.table.setItem(i, j, QTableWidgetItem(rows[i][j]))
-        print(len(rows))
-
-
+            for j in range(len(rows[0])):
+                self.table.setItem(i, j, QTableWidgetItem(str(rows[i][j])))
         # Connection 닫기
         conn.close()
 
-
+    def search(self):
+        self.table.clearContents()
+        conn = pymysql.connect(host='localhost', user='root', password='0000', db='jejudo',
+                               charset='utf8')
+        cur = conn.cursor()
+        data = self.lineEdit.text()
+        data2 = f"%{data}%"
+        sql = "SELECT * FROM jejudo.jeju_table WHERE full_address like %s"
+        cur.execute(sql, data2)
+        rows = cur.fetchall()
+        self.table.setRowCount(len(rows)) # 테이블의 행 갯수를 rows의 길이로 정함
+        self.table.setColumnCount(len(rows[0]))  # 테이블의 열 갯수를 row의 길이로 정함
+        for i in range(len(rows)):
+            for j in range(len(rows[i])):
+                self.table.setItem(i, j, QTableWidgetItem(str(rows[i][j])))
+        conn.commit()
+        conn.close()
 
 
 if __name__ == "__main__":
